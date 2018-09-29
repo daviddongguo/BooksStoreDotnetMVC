9756f95e233f937d1e55ae1978f4c266461c2b66
david.acer <david.dong.guo@gmail.com>
2018-09-29 16:31:55 -0400

    add attribute to product class,  update packages

--- David.BooksStore.Domain/Entities/Product.cs
+++ David.BooksStore.Domain/Entities/Product.cs
@@ -4,17 +4,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace David.BooksStore.Domain.Entities
 {
     public class Product
     {
         [Key]
+        [HiddenInput(DisplayValue = false)]
         public long ProductId { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public decimal Price { get; set; }
         public string CategoryId { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
     }
 }
---
+++ David.BooksStore.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using David.BooksStore.Domain.Abstract;
+using David.BooksStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace David.BooksStore.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+        public AdminController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository
+            .Products
+            .FirstOrDefault(p => p.ProductId == productId);
+            return View(product);
+        }
+    }
+}
