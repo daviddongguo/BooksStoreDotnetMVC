2d9b46b21762fa5fb22f556b153dfeafa23bcf29
david.acer <david.dong.guo@gmail.com>
2018-10-13 21:31:17 -0400

    user linked to database

--- David.BooksStore.WebApp/Infrastructure/Concrete/FormsAuthProvider.cs
+++ David.BooksStore.WebApp/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -1,7 +1,10 @@
-﻿using David.BooksStore.WebApp.Infrastructure.Abstract;
+﻿using David.BooksStore.Domain.Concrete;
+using David.BooksStore.WebApp.Infrastructure.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Security;
 
@@ -9,13 +12,41 @@ namespace David.BooksStore.WebApp.Infrastructure.Concrete
 {
     public class FormsAuthProvider : IAuthProvider
     {
+        public EFDbContext Db { get; set; }
+
+        private static string HashMD5(string text)
+        {
+            var source = Encoding.UTF8.GetBytes(text);
+
+            using(MD5 hasher = MD5.Create())
+            {
+                var result = hasher.ComputeHash(source);
+
+                return Convert.ToBase64String(result);
+            }
+        }
+
         public bool Authenticate(string username, string password)
         {
-            bool result = FormsAuthentication.Authenticate(username, password);
+            bool result = false;
+
+            var user = Db.Users.FirstOrDefault(i => i.Username == username);
+
+            if (user != null)
+            {
+                var inputPwdHash = HashMD5(password);
+
+                if(user.PasswordHash == inputPwdHash)
+                {
+                    result = true;
+                }
+            }
+
             if (result)
             {
                 FormsAuthentication.SetAuthCookie(username, false);
             }
+
             return result;
         }
     }
